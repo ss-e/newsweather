7407195576881e98b0fb18a4b7eca4276c088792
steve <steve@netz.ca>
2020-09-24 21:56:42 -0400

    inet fix

--- module/inet/inet.go
+++ module/inet/inet.go
@@ -102,7 +102,7 @@ func getCurrentInetStatus() {
 			} else {
 				fmt.Println("for item: ", InetDB[i].Name, " length is: ", len(InetDB[i].Status))
 				for y := range feed.Items {
-					fmt.Println("checking item ", y, " with values: ", feed.Items[y])
+					//fmt.Println("checking item ", y, " with values: ", feed.Items[y])
 					now := time.Now()
 					checktime := time.Now()
 					if feed.Items[y].UpdatedParsed != nil {
