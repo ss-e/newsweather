7eb89ae997c19412211a962eee1847a3bbb718af
steve <steve@netz.ca>
2020-09-26 22:47:41 -0400

    wmarq test and ffmpeg error

--- main.go
+++ main.go
@@ -150,7 +150,7 @@ func startup() {
 
 func newCmd() *exec.Cmd {
 	return exec.Command("ffmpeg",
-		"-hide_banner", "-nostats", "-loglevel", "fatal",
+		"-hide_banner", "-nostats", "-loglevel", "error",
 		"-draw_mouse", "0", "-thread_queue_size", "16", "-f", "x11grab", "-s", "1920x1080", "-r", "30", "-i", ":99.0",
 		"-thread_queue_size", "128", "-f", "alsa", "-acodec", "pcm_s32le", "-i", "hw:0,1",
 		"-f", "flv", "-ac", "2", "-ar", "44100",
