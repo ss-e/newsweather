cc595009406c9cab56a39f10c783dccbb79ce487
steve <steve@netz.ca>
2020-10-06 22:53:15 -0400

    attempt to fix chartdata x axis

--- module/finance/finance.go
+++ module/finance/finance.go
@@ -169,7 +169,7 @@ func getStockChartData() {
 				if !ok {
 					fmt.Println("Finance error response to chartdatainfo for ticker item ", StockDB[i].Ticker)
 				} else {
-					//fmt.Println("tdb1 declared: ", tdb1)
+					fmt.Println("tdb1 declared: ", tdb1)
 					temp, err := time.Parse("2006-01-02 15:04", tdb1["date"].(string)+" "+tdb1["minute"].(string))
 					if err != nil {
 						//fmt.Println("for ", StockDB[i].Ticker, ", error parsing stock chart data, date ", err)
@@ -193,7 +193,7 @@ func getStockChartData() {
 					}
 					if err == nil && err3 && err4 && err5 && err6 {
 						//fmt.Println("inputting entry")
-						entry := []float64{float64(date), open, high, low, close}
+						entry := []float64{float64(date) * 1000, open, high, low, close}
 						StockDB[i].Chartdata = append(StockDB[i].Chartdata, entry)
 					}
 				}
