084aa46bc3cf7b740ab4820b2ef49aa262e1f08a
steve <steve@netz.ca>
2020-10-12 11:52:01 -0400

    create infinite loop in startup script

--- update.sh
+++ update.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
-export DISPLAY=:99
-export PKG_CONFIG_PATH=/usr/local/lib64/pkgconfig
-git pull && go build
-rm -rf ~/.cache/newsweather/WebKitCache/
-Xvfb :99 -screen 0 1920x1080x24 &
-./newsweather
-killall ffmpeg
-killall Xvfb
+while :
+do
+    export DISPLAY=:99
+    export PKG_CONFIG_PATH=/usr/local/lib64/pkgconfig
+    git pull && go build
+    rm -rf ~/.cache/newsweather/WebKitCache/
+    Xvfb :99 -screen 0 1920x1080x24 &
+    ./newsweather
+    killall ffmpeg
+    killall Xvfb
+done
