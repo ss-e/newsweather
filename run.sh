6873f9cedc704a98cf50d8260e311401cd28323f
steve <steve@netz.ca>
2021-06-22 22:06:15 -0400

    start css cleanup, run.sh fix

--- run.sh
+++ run.sh
@@ -3,7 +3,7 @@ while :
 do
     export DISPLAY=:99
     export PKG_CONFIG_PATH=/usr/local/lib64/pkgconfig
-    git pull && go build
+    go build
     rm -rf ~/.cache/newsweather/WebKitCache/
     Xvfb :99 -screen 0 1920x1080x24 &
     ./newsweather
